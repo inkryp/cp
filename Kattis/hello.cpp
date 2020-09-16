4cf172d5f6062a69317b548c08d30b69fa47a63b
Axel Villanueva <axelgiovillanueva@icloud.com>
2020-09-16 12:42:26 -0500

    Started book CP4
    Going over as many problems as I can that I feel add value.
    As this is just a simple "Hello World!", thought it would be good as the announcement for starting the book.

---
+++ Kattis/hello.cpp
@@ -0,0 +1,10 @@
+// https://open.kattis.com/problems/hello
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);	cout.tie(0);
+	cout << "Hello World!" << endl;
+	return 0;
+}
