9199512c996dfc7eb8c76915beae61975f52c031
Axel Villanueva <axelgiovillanueva@icloud.com>
2020-12-19 04:45:37 -0600

    Slight change to the template

--- misc/template/template.cpp
+++ misc/template/template.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);	cout.tie(0);
-	/*cout.setf(ios::fixed);
-	cout.precision(10);*/
+	// cout.setf(ios::fixed);
+	// cout.precision(10);
 	
 	return 0;
 }
